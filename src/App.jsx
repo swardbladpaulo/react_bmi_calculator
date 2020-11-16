156886cee9db38af9af8dfdc4736686b6c0cd7e3
swardbladpaulo <paulo@swardblads.se>
2020-11-16 18:00:07 +0100

    modified app.jsx, form.jsx. the test dont like height on row 7 in app.jsx, test failed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Form from './components/Form';
 
 class App extends Component {
+  state = {
+    weight: ""
+    height: ""
+  };
+
+  onChangeHandler = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
-    return <Form />
+    return (
+      <div>
+        <Form
+          weight={this.state.weight}
+          height={this.state.height}
+          onChangeHandler={this.onChangeHandler}
+          />
+      </div>
+    );
   }
 }
 
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,16 +8,20 @@ const Form = props => {
         type="number"
         required
         placeholder="Weight in kgs"
+        value={props.weight}
         name="weight"
         id="weight"
+        onChange={props.onChangeHandler}
       />
       <label htmlFor="height">Height</label>
       <input
         type="number"
         required
         placeholder="Height in cm"
+        value={props.height}
         name="height"
         id="height"
+        onChange={props.onChangeHandler}
       />
       <button>Calculate BMI</button>
     </form>
