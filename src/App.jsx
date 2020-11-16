769d08a6d421573ed4e76cf79e0a14498432b2c5
swardbladpaulo <paulo@swardblads.se>
2020-11-16 17:00:33 +0100

    added form to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,27 @@ import React, { Component } from 'react'
 
 class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <form>
+        <label htmlFor="weight">Weight</label>
+        <input
+          type="number"
+          required
+          placeholder="Weight in kgs"
+          name="weight"
+          id="weight"
+        />
+        <label htmlFor="height">Height</label>
+        <input
+          type="number"
+          required
+          placeholder="Height in cm"
+          name="height"
+          id="height"
+        />
+        <button>Calculate BMI</button>
+      </form>
+    );
   }
 }
 
